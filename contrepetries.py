cde6aeac5f3177c03d94e0182c6d4d48b3210757
Marty <martin.kirch@gmail.com>
2011-07-05 14:18:21 +0200

    encore de la contrepetrie

--- contrepetries.py
+++ contrepetries.py
@@ -12,6 +12,11 @@ contrepetries = [
     , ("La Chine s'élève à l'approche des Nippons")
     , ("Il fait beau et chaud")
     , ("Elle revenait de la ferme pleine d'espoir jusqu'au pont du Juras")
+    , ("Je suis arrivé à pied par la Chine")
+    , ("Un dernier calcul et on s'en va !")
+    , ("La berge du ravin est couverte de végétation")
+    , ("Salut Patrick")
+    , ("J'ai bien peur d'avoir une panne de micro")
 ]
 
 if __name__ == "__main__":
