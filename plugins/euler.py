9fd1a5effa9939fb1ebfa56e1408cbe3fd10a82b
Etienne Millon <etienne.millon@gmail.com>
2011-10-26 11:14:26 +0200

    euler : return scores instead of updating them

--- plugins/euler.py
+++ plugins/euler.py
@@ -9,15 +9,17 @@ class PluginEuler(Plugin):
         self._eulerNicks = set()
 
     def euler_update_data(self):
+        scores = {}
         for nick in self._eulerNicks:
             url = "http://projecteuler.net/profile/%s.txt" % nick
             s = urllib2.urlopen(url).read().split(',')
             if(len(s) >= 4):
-                self._eulerScores[nick] = s[3]
+                scores[nick] = s[3]
+        return scores
 
     @cmd(0)
     def cmd_euler(self, chan, args):
-        self.euler_update_data()
+        self._eulerScores = self.euler_update_data()
         for nick, score in self._eulerScores.items():
             self.say("%s : %s" %(nick, score))
 
