fe2a54604b96974c9ae4ea69dbe7603a5e393807
Etienne Millon <etienne.millon@gmail.com>
2011-10-28 15:27:26 +0200

    robust lol-regexp

--- plugins/lolrate.py
+++ plugins/lolrate.py
@@ -40,7 +40,7 @@ class PluginLolrate(Plugin):
         bot._mutable_attributes['lolRateDepth'] = int
 
     def handle_msg(self, msg_text, chan, nick):
-        lulz = len(re.findall("[Ll]+[oO0]+[Ll]+", msg_text))
+        lulz = len(re.findall("[lI1]+[o0]+[lI1]+", msg_text, flags=re.IGNORECASE))
         if lulz > 0:
             ts = TimeSlice()
             if ts != self.lolRate[0]:
