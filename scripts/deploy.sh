638c4796df99121944fcb0572ce998ecbc2a6d6c
Etienne Millon <etienne.millon@gmail.com>
2012-02-02 17:44:08 +0100

    Add deployment script

---
+++ scripts/deploy.sh
@@ -0,0 +1,39 @@
+# This script will be sourced with the following variables set
+# 
+# TOF_HOME    location of the git repository
+# TOF_CONFIG  configuration file
+# GIT_REMOTE  name of remote where to pull new objects
+# SSD         start-stop-daemon with pid argument
+# action      in update,start,stop
+
+usage() {
+        echo RTFM
+        exit 1
+}
+
+if [ $# -lt 1 ]
+then
+        usage
+fi
+
+case "$action" in
+        update )
+                cd "$TOF_HOME"
+                git pull "$GIT_REMOTE" master
+                ;;
+        start )
+		find "$TOF_HOME" -name '*.pyc' -delete
+                $SSD \
+                        --chdir $TOF_HOME \
+                        --background \
+                        --make-pidfile \
+                        --exec $TOF_HOME/bot.py \
+                        -S -- -x "$TOF_CONFIG"
+                ;;
+        stop )
+                $SSD -K
+                ;;
+        * )
+                usage
+                ;;
+esac
