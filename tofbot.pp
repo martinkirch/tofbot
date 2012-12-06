f656d541f6eded9095dae7123d1b97d954286151
Etienne Millon <me@emillon.org>
2012-12-06 12:47:14 +0100

    Puppet conf

---
+++ tofbot.pp
@@ -0,0 +1,29 @@
+user {'tofbot':
+    ensure => present,
+}
+
+vcsrepo {'/usr/local/src/tofbot':
+    ensure   => latest,
+    provider => git,
+    source   => 'https://github.com/martinkirch/tofbot.git',
+    revision => 'master',
+}
+
+file {'tofbot-init':
+    path      => '/etc/init.d/tofbot',
+    source    => '/usr/local/src/tofbot/tofbot.init',
+    subscribe => Vcsrepo['/usr/local/src/tofbot'],
+}
+
+file {'tofbot-defaults':
+    path    => '/etc/default/tofbot',
+    content => '# CONFFILE= path to tofbot config',
+}
+
+service {'tofbot':
+    ensure    => running,
+    require   => User['tofbot'],
+    subscribe => [File['tofbot-init'],
+                  Vcsrepo['/usr/local/src/tofbot'],
+    ],
+}
