f9cc6d09c12c5703230686c4294f24ab07b06807
Etienne Millon <me@emillon.org>
2012-12-20 19:09:56 +0100

    Test rick

--- testbot.py
+++ testbot.py
@@ -206,3 +206,16 @@ class TestCase(unittest.TestCase):
     def test_sed_invalid(self):
         self.bot.send("oho")
         self.assertNoOutput("s/++/a")
+
+    @httprettified
+    def test_rick(self):
+        url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        title = 'rickroll'
+        response = '<html><head><title>%s</title></head></html>' % title
+        HTTPretty.register_uri(HTTPretty.GET, url,
+                               body=response,
+                               )
+        self.assertOutput("Keyboard cat v2: %s" % url,
+                          [title,
+                           "We're no strangers to love..."
+                           ])
