73f0a379fcb408f167d8e98755e59d3843fa268f
Etienne Millon <me@emillon.org>
2012-12-30 11:03:40 +0100

    test expand for errors

--- testbot.py
+++ testbot.py
@@ -183,6 +183,28 @@ class TestCase(unittest.TestCase):
                                )
         self.assertOutput("Check out this video %s" % url, title)
 
+    @httprettified
+    def test_expand_video_error(self):
+        """
+        If youtube returns an unparseable page, don't bail out.
+        """
+        url = 'https://www.youtube.com/watch?v=J---aiyznGQ'
+        HTTPretty.register_uri(HTTPretty.GET, url, body='',)
+        self.assertOutputLength("Check out %s" % url, 0)
+
+    @httprettified
+    def test_expand_mini_error(self):
+        """
+        If a minifier creates a redirect loop, don't bail out.
+        """
+        url = 'http://tinyurl.com/deadbeef'
+        target = url
+        HTTPretty.register_uri(HTTPretty.GET, url,
+                               status=301,
+                               location=target,
+                               )
+        self.assertOutputLength("Check out %s" % url, 0)
+
     def test_jokes_autotofade(self):
         (event_k, event) = self._find_event(TofadeEvent)
 
